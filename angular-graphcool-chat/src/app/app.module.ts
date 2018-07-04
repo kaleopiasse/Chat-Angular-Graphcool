2faf7a2dd4908f51265ec1d16ff6bda7ede50de2
Kaleo Piasse <kaleopiasse@gmail.com>
2018-07-04 01:05:39 -0300

    First commit

---
+++ angular-graphcool-chat/src/app/app.module.ts
@@ -0,0 +1,19 @@
+import { NgModule } from '@angular/core';
+import {MatToolbarModule, MatListModule} from '@angular/material';
+
+import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
+@NgModule({
+  declarations: [
+    AppComponent
+  ],
+  imports: [
+    BrowserAnimationsModule,
+    MatToolbarModule,
+    MatListModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }

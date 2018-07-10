b5d98eefaf6aa11e30d2abf6ac65d77e15d91cfb
Kaleo A. Piasse <kaleopiasse@gmail.com>
2018-07-10 15:22:12 -0300

    Validação de erros front-end, create user

--- angular-graphcool-chat/src/app/login/components/login/login.component.ts
+++ angular-graphcool-chat/src/app/login/components/login/login.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { FormGroup, FormBuilder } from '@angular/forms';
+import { FormGroup, FormBuilder, Validators, FormControl } from '@angular/forms';
 
 @Component({
   selector: 'app-login',
@@ -9,6 +9,14 @@ import { FormGroup, FormBuilder } from '@angular/forms';
 export class LoginComponent implements OnInit {
 
   loginForm: FormGroup;
+  configs = {
+    isLogin: true,
+    actionText: 'SignIn',
+    buttonActionText: 'Create account'
+  };
+
+  private nameControl = new FormControl('', [Validators.required, Validators.minLength(5)]);
+
   constructor(
     private formBuilder: FormBuilder
   ) { }
@@ -19,12 +27,23 @@ export class LoginComponent implements OnInit {
 
   createForm(): void {
     this.loginForm = this.formBuilder.group({
-      email: '',
-      password: ''
+      email: ['', [Validators.required, Validators.email]],
+      password: ['', [Validators.required, Validators.minLength(4)]]
     });
   }
 
   onSubmit() {
     console.log(this.loginForm.value);
   }
+
+  changeAction(): void {
+    this.configs.isLogin = !this.configs.isLogin;
+    this.configs.actionText = !this.configs.isLogin ? 'SignUp' : 'SignIn';
+    this.configs.buttonActionText = !this.configs.isLogin ? 'Already have account' : 'Create account';
+    !this.configs.isLogin ? this.loginForm.addControl('name', this.nameControl) : this.loginForm.removeControl('name');
+  }
+
+  get email(): FormControl { return <FormControl>this.loginForm.get('email'); }
+  get password(): FormControl { return <FormControl>this.loginForm.get('password'); }
+  get name(): FormControl { return <FormControl>this.loginForm.get('name'); }
 }
